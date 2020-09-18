9b71a19dfcdd11191ecb2ac4dece3abf5c6e3254
Renata Sechi <sechi@zib.de>
2020-09-18 16:15:26 +0200

    new ideas for the spectra

--- method_density.py
+++ method_density.py
@@ -12,6 +12,7 @@ from scipy.linalg import svd, pinv
 from scipy.optimize import fmin
 import cmdtools
 from tools import norm_rows, avg_spectrum
+import cmdtools.estimation.voronoi as voronoi
 #%%
 
 
@@ -28,6 +29,12 @@ def three_states_system(M):
             else:
                 M_copy[i , j] = 3.
     return(M_copy)
+    
+    return(ind_matrix)
+#def sum_trafo(M, centers):
+#    sum_to = np.zeros()
+#    for i in range(M.shape[1]):
+        
 def countmatrix(M, avg=1, tau=1):
     M = norm_rows(M, avg)
     tau = int(tau)
@@ -39,7 +46,8 @@ def countmatrix(M, avg=1, tau=1):
 #%%
 #trialmtx = np.loadtxt('iso_br_al_cor_py2_400nm_ex_ir.txt')[1:,1:]
 data = np.loadtxt("br_py2_exec400.txt")
-trialmtx = data[1:,1:]
+#data = np.loadtxt('iso_br_al_cor_py2_400nm_ex_ir.txt')
+trialmtx = data[42:,1:]
 red_mtx = three_states_system(0.15*trialmtx[:,:])
 #trialmtxpos = trialmtx-np.amin(trialmtx)
 plt.figure(figsize=(12,4))
@@ -48,13 +56,13 @@ plt.imshow(trialmtx[:,:], cmap='inferno', aspect='auto')
 plt.colorbar()
 plt.title("spectrum")
 plt.xticks(np.arange(len(data[0,1:]), step=50),labels=np.round(data[0,1::50],1))
-plt.yticks(np.arange(len(data[1:,0]), step=20),labels=np.round(data[1::20,0],1))
+plt.yticks(np.arange(len(data[42:,0]), step=20),labels=np.round(data[42::20,0],1))
 plt.subplot(1, 2, 2)
 plt.imshow(red_mtx, cmap="inferno_r",aspect = "auto")
 plt.colorbar()
 plt.title("reduced three states system")
 plt.xticks(np.arange(len(data[0,1:]), step=50),labels=np.round(data[0,1::50],1))
-plt.yticks(np.arange(len(data[1:,0]), step=20),labels=np.round(data[1::20,0],1))
+plt.yticks(np.arange(len(data[42:,0]), step=20),labels=np.round(data[42::20,0],1))
 plt.show()
 count_tm = countmatrix(red_mtx[101:,:], 1,1)
 plt.imshow(count_tm)
@@ -85,3 +93,5 @@ Pc = np.linalg.pinv(Chi).dot(count_tm.dot(Chi))
 #%%
 diff_lambda = abs(data[0,1:-1]-data[0,2:])
 plt.plot(np.arange(len(diff_lambda)),diff_lambda, "-o")
+#%%
+t_o = voronoi.VoronoiTrajectory(np.reshape(red_mtx[:,3], (139,1)), 1, centers=np.array([[1,2,3]]).T)
