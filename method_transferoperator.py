c0b263c963fdd077f4fa761c797873ba991690fd
Renata Sechi <sechi@zib.de>
2020-09-21 14:52:42 +0200

    method transfer operator, clean method density

--- method_transferoperator.py
+++ method_transferoperator.py
@@ -14,6 +14,7 @@ import cmdtools
 from tools import norm_rows, avg_spectrum
 import cmdtools.estimation.voronoi as voronoi
 from cmdtools import utils
+from cmdtools import p
 import cmdtools.estimation.picking_algorithm as picking_algorithm
 #%%
 
@@ -49,7 +50,7 @@ def countmatrix(M, avg=1, tau=1):
 #trialmtx = np.loadtxt('iso_br_al_cor_py2_400nm_ex_ir.txt')[1:,1:]
 data = np.loadtxt("br_py2_exec400.txt")
 #data = np.loadtxt('iso_br_al_cor_py2_400nm_ex_ir.txt')
-trialmtx = data[42:,1:]
+trialmtx = data[40:,1:]
 red_mtx = three_states_system(0.15*trialmtx[:,:])
 #trialmtxpos = trialmtx-np.amin(trialmtx)
 plt.figure(figsize=(12,4))
@@ -58,58 +59,48 @@ plt.imshow(trialmtx[:,:], cmap='inferno', aspect='auto')
 plt.colorbar()
 plt.title("spectrum")
 plt.xticks(np.arange(len(data[0,1:]), step=50),labels=np.round(data[0,1::50],1))
-plt.yticks(np.arange(len(data[42:,0]), step=20),labels=np.round(data[42::20,0],1))
+plt.yticks(np.arange(len(data[40:,0]), step=20),labels=np.round(data[40::20,0],1))
 plt.subplot(1, 2, 2)
 plt.imshow(red_mtx, cmap="inferno_r",aspect = "auto")
 plt.colorbar()
 plt.title("reduced three states system")
 plt.xticks(np.arange(len(data[0,1:]), step=50),labels=np.round(data[0,1::50],1))
-plt.yticks(np.arange(len(data[42:,0]), step=20),labels=np.round(data[42::20,0],1))
+plt.yticks(np.arange(len(data[42:,0]), step=20),labels=np.round(data[40::20,0],1))
 plt.show()
-count_tm = countmatrix(red_mtx[101:,:], 1,1)
-plt.imshow(count_tm)
-plt.title("count matrix from 3 states system, $\tau=1$")
+
+
+#%%
+thing = picking_algorithm.picking_algorithm(red_mtx,10 )
+trafo_pa = voronoi.VoronoiTrajectory(red_mtx, red_mtx.shape[1], centers=red_mtx[::10,:]).propagator()
+#%%
+
+plt.imshow(trafo_pa)
+plt.title("transfer matrix from 3 states system, $\tau=1$")
 plt.colorbar()
 plt.show()
 #%%
-#mtx2 = trialmtx.T.dot(trialmtx)
-#plt.imshow(countmatrix(mtx2, avg=3))
 
-#plt.show()
-#%%
-Chi = cmdtools.pcca(count_tm, 4)
-#plt.imshow(chi_k, cmap="inferno", aspect= "auto", interpolation= "nearest")
-#plt.title("$\chi$ from count matrix")
+#temp = np.zeros(red_mtx.shape)
+#for i in thing[1]:
+#    temp[i,:] = red_mtx[i,:]
+#    #%%
+#plt.imshow(red_mtx+2*temp, cmap="tab10",aspect = "auto")
 #plt.colorbar()
-#plt.show()
-for i in range(len(Chi.T)):
-    plt.plot(Chi.T[i], label=i)
-    plt.xticks(np.arange(len(data[0,1:]), step=50),labels=np.round(data[0,1::50],1), label=i)
-    plt.legend()
-    plt.xlim(400,100)
-    plt.xlabel("$\lambda/nm$")
-    plt.title("$\chi$")
-    plt.show()
-#%%
-Pc = np.linalg.pinv(Chi).dot(count_tm.dot(Chi))
-#%%
-diff_lambda = abs(data[0,1:-1]-data[0,2:])
-plt.plot(np.arange(len(diff_lambda)),diff_lambda, "-o")
-#%%
-cc = np.reshape(np.array([1.,2.,3.]), (3,1))
-first_to = np.zeros((3,3))
-for i in range(red_mtx.shape[1]):
-    t_o = voronoi.VoronoiTrajectory(np.reshape(red_mtx[:,i], (139,1)), 1).propagator()
-    first_to = np.sum([first_to, t_o], axis=0)
+##lt.imshow(temp, cmap="gray",aspect = "auto")
     
-first_to = utils.rowstochastic(first_to)
 
+transferop = voronoi.VoronoiTrajectory(trialmtx, trialmtx.shape[1], centers=trialmtx[::10,:]).propagator()   
 #%%
-X = np.random.rand(100, 5)
-centers = np.random.rand(10, 5)
-ppp = voronoi.VoronoiTrajectory(X, 5, centers=centers)
+ew, ev = np.linalg.eig(transferop)
+#%%
+plt.figure(figsize=(12,4))
+plt.subplot(1, 2, 1)
+plt.imshow(transferop, cmap='inferno_r', aspect='auto')
+plt.colorbar()
 
+plt.subplot(1, 2, 2)
+plt.imshow(trafo_pa, cmap="inferno_r",aspect = "auto")
+plt.colorbar()
+plt.show()
 #%%
-thing = picking_algorithm.picking_algorithm(red_mtx,50 )
-trafo_pa = voronoi.VoronoiTrajectory(red_mtx, red_mtx.shape[1], centers=thing[0]).propagator()
-#%%
+a = cmdtools.analysis.pcca.scipyschur(trafo_pa, 3)
